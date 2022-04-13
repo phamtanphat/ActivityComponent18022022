c8997882eb0c56fc88ffde9fbf4cdef9aebade42
Phat <phatdroid94@gmail.com>
2022-04-13 19:10:50 +0700

    single task

--- app/src/main/java/com/example/activitycomponent18022022/MainActivity3.java
+++ app/src/main/java/com/example/activitycomponent18022022/MainActivity3.java
@@ -23,7 +23,7 @@ public class MainActivity3 extends AppCompatActivity {
         mBtnNavigateScreen3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity3.this,MainActivity3.class);
+                Intent intent = new Intent(MainActivity3.this,MainActivity.class);
                 startActivity(intent);
             }
         });
